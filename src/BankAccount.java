68e6080bd040733bf39bd30e6f81547b3f0199a3
Cameron Borrego <161384565+Cameron-Rey@users.noreply.github.com>
2025-01-31 00:19:24 +0000

    lab 03 commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,34 @@
+public class BankAccount {
 
+
+    public static void main(String[] args) {
+
+        float myBalance = 2175.37f;
+        System.out.println("Bob's checking account balance: " + myBalance);
+
+        
+        System.out.println("Bob withdrew: $302.50");
+        myBalance -= 302.50;
+
+        System.out.println("Bob deposited $50.03");
+        myBalance += 50.03;
+
+        System.out.println("Bob withdrew: $961.45 & deposited $20.00");
+        myBalance /= 2;
+        myBalance += 20;
+        
+        System.out.println("Bob withdrew: $1.00");
+        --myBalance;
+
+        System.out.println("Bob deposited: $980.45");
+        myBalance *= 2;
+
+        System.out.println("Bob deposited: $1.00");
+        ++myBalance;
+
+        System.out.println("Bob's remaining balance: $" + String.format("%.2f", myBalance));
+        
+
+    }
+
+}
